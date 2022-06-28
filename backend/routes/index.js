a5130572f690dc4aea48656e238c61bf20909060
Fajar <fajarmh2@gmail.com>
2022-06-28 15:35:49 +0700

    adding dependency

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,6 +2,7 @@ const express = require("express")
 const { addBalance } = require("../controllers/v1/canteen_balance_box_controller.js");
 const router = express.Router();
 
+// Balance Canteen Box
 router.get("/balance", addBalance);
 
 module.exports = router;
