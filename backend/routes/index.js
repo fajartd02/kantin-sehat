297ba69c19bf32e9cf7a9da86be5d676b3f785b8
Fajar <fajarmh2@gmail.com>
2022-06-28 16:23:20 +0700

    Create service balance box

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,6 +3,6 @@ const { addBalance } = require("../controllers/v1/canteen_balance_box_controller
 const router = express.Router();
 
 // Balance Canteen Box
-router.get("/balance", addBalance);
+router.post("/api/v1/balance", addBalance);
 
 module.exports = router;
--- backend/services/canteen_balance_box_service.js
+++ backend/services/canteen_balance_box_service.js
@@ -1,13 +1,27 @@
-const CanteenBalanceBox = require('../models/canteen_balance_box_model.js');
+const { CanteenBalanceBox } = require('../models');
 
 class CanteenBalanceBoxService {
-	async addingMoneyToCanteen(id, balance) {
-		const money = await CanteenBalanceBox.findOne({
-			where: {
-				id: id
-			}
-		});
-		
+	async addingMoneyToCanteen(id, newBalance) {
+		const canteen = await CanteenBalanceBox.findOne({where: {id}});
+		console.log(canteen);
+
+		if(canteen == null) {
+			await CanteenBalanceBox.create({balance: 0});
+			canteen = await CanteenBalanceBox.findOne({where: {id}});
+		}
+
+		let { balance } = canteen;
+		let totalBalance = balance + newBalance;
+
+		await CanteenBalanceBox.update({balance: totalBalance}, {where: {id}});
+
+		return {
+			message_developer: "Successfully added balance",
+			status_code: 400,
+			before_added_balance: balance,
+			added_balance: newBalance,
+			after_added_balance: totalBalance
+		}
 	}
 
 }
