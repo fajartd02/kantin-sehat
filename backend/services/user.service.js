af01138405f6a267d687fdbdf3bc667c579b9c7b
Fajar Muhammad Hamka <fajarmh2@gmail.com>
2022-06-30 16:26:54 +0700

    Create register account, logic belum

--- backend/services/user.service.js
+++ backend/services/user.service.js
@@ -1,39 +1,37 @@
 const { User } = require('../models');
+const bcrypt = require('bcrypt');
+const jwt = ('jsonwebtoken');
 
 class UserService {
 	async register(student_id, password) {
     const isUserExist = await User.findOne({where: {student_id}});
 
     if(isUserExist) {
-      return alreadyExist();
+      return alreadyExist;
     }
 
     const salt = await bcrypt.genSalt();
     const hashPassword = await bcrypt.hash(password, salt);
 
-    await User.create({student_id, password});
-    return successfullyAdded();
+    await User.create({student_id, password: hashPassword});
+    return successfullyAdded;
   }
 }
 
-function alreadyExist() {
-  return {
-    meta: {
-      message_developer: "User Already Exist",
-      status_code: 409
-    }, 
-    response: null
-  }
+const alreadyExist = {
+  meta: {
+    message_developer: "User Already Exist",
+    status_code: 409
+  }, 
+  response: null
 }
 
-function successfullyAdded() {
-  return {
-    meta: {
-      message_developer: "Successfully added account",
-			status_code: 400
-    },
-    response: null
-  }
+const successfullyAdded = {
+  meta: {
+    message_developer: "Successfully added account",
+		status_code: 400
+  },
+  response: null
 }
 
 module.exports = new UserService();
