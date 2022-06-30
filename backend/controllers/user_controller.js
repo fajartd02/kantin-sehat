4514b3352cf012870f97c30a24e62f80b09f0809
Fajar Muhammad Hamka <fajarmh2@gmail.com>
2022-06-30 16:18:26 +0700

    Create register auth service and controller without checking

--- backend/controllers/user_controller.js
+++ backend/controllers/user_controller.js
@@ -1,11 +1,12 @@
-const { errorInternal } = require('../utils/error_message');
 const UserService = require('../services/user.service.js');
+const { errorInternal } = require('../utils/error_message');
 
 module.exports = {
   register: async(req, res) => {
     const { student_id, password } = req.body;
     try {
-      UserService.register(student_id, password);
+      const response = await UserService.register(student_id, password);
+      return res.json(response);
     } catch(err) {
       return res.send(errorInternal);
     }
--- backend/services/user.service.js
+++ backend/services/user.service.js
@@ -2,7 +2,37 @@ const { User } = require('../models');
 
 class UserService {
 	async register(student_id, password) {
-    
+    const isUserExist = await User.findOne({where: {student_id}});
+
+    if(isUserExist) {
+      return alreadyExist();
+    }
+
+    const salt = await bcrypt.genSalt();
+    const hashPassword = await bcrypt.hash(password, salt);
+
+    await User.create({student_id, password});
+    return successfullyAdded();
+  }
+}
+
+function alreadyExist() {
+  return {
+    meta: {
+      message_developer: "User Already Exist",
+      status_code: 409
+    }, 
+    response: null
+  }
+}
+
+function successfullyAdded() {
+  return {
+    meta: {
+      message_developer: "Successfully added account",
+			status_code: 400
+    },
+    response: null
   }
 }
 
