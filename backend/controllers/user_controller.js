ae900dfefe2cc380c5622f85e277ac9cfd00afed
Fajar <fajarmh2@gmail.com>
2022-07-03 22:18:59 +0700

    create service login

--- backend/controllers/user_controller.js
+++ backend/controllers/user_controller.js
@@ -15,6 +15,12 @@ module.exports = {
     const { student_id, password } = req.body;
     try {
       const response = await UserService.login(student_id, password);
+
+      res.cookie('refreshToken', response.refresh_token, {
+        httpOnly: true, // this mean can't access by client
+        maxAge: 24 * 60 * 60 * 1000 // this mean 1 day
+      });
+
       return res.json(response);
     } catch(err) {
       return res.end(errorInternal);
--- backend/services/user.service.js
+++ backend/services/user.service.js
@@ -1,40 +1,92 @@
 const { User } = require('../models');
 const bcrypt = require('bcrypt');
-const jwt = ('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 
 class UserService {
-	async register(student_id, password) {
-    const isUserExist = await User.findOne({where: {student_id}});
+  async register(student_id, password) {
+    const isUserExist = await User.findOne({ where: { student_id } });
 
-    if(isUserExist) {
+    if (isUserExist) {
       return alreadyExist;
     }
 
-    if(!validateId(student_id)) {
+    if (!validateId(student_id)) {
       return invalidId;
     }
 
     const salt = await bcrypt.genSalt();
     const hashPassword = await bcrypt.hash(password, salt);
 
-    await User.create({student_id, password: hashPassword});
+    await User.create({ student_id, password: hashPassword });
     return successfullyAdded;
-    
   }
+
+  async login(student_id, password) {
+    const user = await User.findOne({ where: { student_id: student_id } });
+    if(!user) {
+      return accountNotExist;
+    }
+
+    const matchPassword = await bcrypt.compare(password, user.password);
+    if (!matchPassword) {
+      return notMatchPassword;
+    }
+
+    const userId = user.student_id;
+    const accessToken = jwt.sign({ userId }, process.env.SECRET_ACCESS_TOKEN, {
+      expiresIn: '60s'
+    });
+
+    const refreshToken = jwt.sign({ userId }, process.env.SECRET_REFRESH_TOKEN, {
+      expiresIn: '1d'
+    });
+
+    console.log(accessToken);
+    console.log(refreshToken);
+
+    await User.update({ refresh_token: refreshToken }, {
+      where: {
+        student_id: userId
+      }
+    });
+
+    return sucessfullyLogin(accessToken);
+  }
+}
+
+const sucessfullyLogin = (accessToken, refresh_token) => {
+  return {
+    meta: {
+      message_developer: "Successfully login",
+      status_code: 400
+    },
+    response: {
+      accessToken,
+      refresh_token
+    }
+  }
+}
+
+const notMatchPassword = {
+  meta: {
+    message_developer: "Password not match",
+    status_code: 401
+  },
+  response: null
 }
 
 const alreadyExist = {
   meta: {
     message_developer: "User Already Exist",
     status_code: 409
-  }, 
+  },
   response: null
 }
 
 const successfullyAdded = {
   meta: {
     message_developer: "Successfully added account",
-		status_code: 400
+    status_code: 400
   },
   response: null
 }
@@ -42,7 +94,15 @@ const successfullyAdded = {
 const invalidId = {
   meta: {
     message_developer: "ID not valid",
-		status_code: 422
+    status_code: 422
+  },
+  response: null
+}
+
+const accountNotExist = {
+  meta: {
+    message_developer: "Account not exist!",
+    status_code: 401
   },
   response: null
 }
@@ -52,11 +112,11 @@ function validateId(student_id) {
   const twoLastDigits = student_id.substring(3, 5);
 
   let sumOfThreeFirstDigits = 0;
-  for(let i = 0; i < threeFirstDigits.length; i++) {
+  for (let i = 0; i < threeFirstDigits.length; i++) {
     sumOfThreeFirstDigits += parseInt(threeFirstDigits[i]);
   }
 
-  if(sumOfThreeFirstDigits !== parseInt(twoLastDigits)) {
+  if (sumOfThreeFirstDigits !== parseInt(twoLastDigits)) {
     return false;
   }
   return true;
