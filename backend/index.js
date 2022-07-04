0fab639cd068c171a793c3b647706ebadfbc9fc4
Fajar Muhammad Hamka <fajarmh2@gmail.com>
2022-07-04 19:22:31 +0700

    adding cors

--- backend/index.js
+++ backend/index.js
@@ -26,6 +26,7 @@ async function connectToDatabase() {
 
 function activatedMiddleware() {
   // app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
+  app.use(cors());
   app.use(cookieParser());
   app.use(express.json());
   app.use(router);
