85f15c2bdaa7ae65a4529a2107ebac0f9acaf457
Fajar <fajarmh2@gmail.com>
2022-07-04 15:07:12 +0700

    Create seed user

---
+++ backend/seeders/20220704080233-user.js
@@ -0,0 +1,33 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('users',
+      [
+        {
+          student_id: "45615",
+          password: "$2b$10$3t9PSeGUh.4ujH3bdQcE2.rkFPMlyOG.Ir/72w5Q7EhmN6CJ4HsZK", // 123456
+          refresh_token: null,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          student_id: "13307",
+          password: "$2b$10$3t9PSeGUh.4ujH3bdQcE2.rkFPMlyOG.Ir/72w5Q7EhmN6CJ4HsZK", // 123456
+          refresh_token: null,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        }
+      ], {});
+
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
