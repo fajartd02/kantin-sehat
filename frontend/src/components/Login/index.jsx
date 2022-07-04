470221a1742e1616d4c3b3831d2e12c7450d78ca
Fajar Muhammad Hamka <fajarmh2@gmail.com>
2022-07-04 21:35:27 +0700

    Create login page

--- frontend/src/components/Login/index.jsx
+++ frontend/src/components/Login/index.jsx
@@ -3,67 +3,60 @@ import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
 
 function Login() {
-  const [email, setEmail] = useState('');
+  const [studentId, setStudentId] = useState('');
   const [password, setPassword] = useState('');
-  const [msg, setMsg] = useState('');
+  const [message, setMessage] = useState('');
   const navigate = useNavigate();
 
   const Auth = async (e) => {
-    e.preventDefault(); // agar page tidak reload
-    try {
-      await axios.post('http://localhost:5000/login', {
-        email: email,
+    e.preventDefault(); 
+    const response = await axios.post('http://localhost:8080/auth/login', {
+        student_id: studentId,
         password: password,
-      });
-
-      navigate("/dashboard");
-    } catch (err) {
-      if (err.response) {
-        setMsg(err.response.data.message);
-      }
+    });
+    const { status_code } = response.data.meta;
+    const notExist = 401;
+    if(status_code === notExist) {
+      setMessage("Please enter correct username and password!");
+    } else {
+      navigate("/");
     }
   }
   return (
-    <section className="hero has-background-grey-light is-fullheight is-fullwidth">
-      <div className="hero-body">
-        <div className="container">
-          <div className="columns is-centered">
-            <div className="columns is-4-desktop">
-              <form className="box" onSubmit={Auth}>
-                <p className='has-text-centered has-text-danger'>{msg}</p>
-                <div className="field mt-2">
-                  <label className='label'>Email or Username</label>
-                  <div className="controls">
-                    <input
-                      type="text"
-                      className='input'
-                      placeholder='Username'
-                      value={email}
-                      onChange={(e) => setEmail(e.target.value)}
-                    />
-                  </div>
-                </div>
-                <div className="field mt-5">
-                  <label className='label'>Password</label>
-                  <div className="controls">
-                    <input type="password"
-                      className='input'
-                      placeholder='******'
-                      value={password}
-                      onChange={(e) => setPassword(e.target.value)}
-                    />
-                  </div>
-                </div>
-                <div className="field mt-5">
-                  <button className='button is-success is-fullwidth'>Login</button>
-                </div>
-                <Link to="/register">Dont have an account? <u>Regist here</u></Link>
-              </form>
-            </div>
+    <div className='container text-center' style={{marginTop: "12.5%"}}>
+    <h2 className='mb-4 text-success'>Login</h2>
+      <form className="box" onSubmit={Auth}>
+        <p className='text-danger'>{message}</p>
+        <div className="field mt-2">
+          <label className='label'>Student ID</label>
+          <div className="controls">
+            <input
+              type="text"
+              className='input'
+              placeholder='Username'
+              value={studentId}
+              onChange={(e) => setStudentId(e.target.value)}
+            />
           </div>
         </div>
-      </div>
-    </section>
+        <div className="field mt-5">
+          <label className='label'>Password</label>
+          <div className="controls">
+            <input type="password"
+              className='input'
+              placeholder='******'
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+        </div>
+        <div className="field mt-5">
+          <button className='btn btn-lg btn-success mb-3'>Login</button>
+        </div>
+        <Link to="/register">Dont have an account? <u>Regist here</u></Link>
+      </form>
+    </div>
+
   );
 }
 
