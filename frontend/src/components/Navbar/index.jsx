cbd69c4fe9b0fb12c17dd6850100635f81885ea1
Fajar Muhammad Hamka <fajarmh2@gmail.com>
2022-07-04 19:19:40 +0700

    adding routes link navbar

--- frontend/src/components/Navbar/index.jsx
+++ frontend/src/components/Navbar/index.jsx
@@ -1,11 +1,29 @@
 import React from 'react'
+import { Link } from "react-router-dom";
+
 
 function Navbar() {
   return (
-    <div className='container'>
-      <h1>Hello World!</h1>
-      
-    </div>
+    <>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div className='container'>
+          <a class="navbar-brand" href="#">Kantin Sehat</a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+              <Link to="/" style={{ textDecoration: "none" }}>
+                  <a className="nav-link active">Home</a>
+              </Link>
+              <Link to="/balance" style={{ textDecoration: "none" }}>
+                  <a className="nav-link active">Balance Box</a>
+              </Link>
+            </div>
+          </div>
+        </div>
+    </nav>
+    </>
   )
 }
 
