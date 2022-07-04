f2c693e5b779b339d195f82968635e495e9b27c8
Fajar <fajarmh2@gmail.com>
2022-07-04 15:20:37 +0700

    add seeding product

---
+++ backend/seeders/20220704081848-product.js
@@ -0,0 +1,36 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('products',
+      [
+        {
+          student_id: "45615",
+          product_name: "T-shirt T-rex",
+          product_image: "src/assets/img/2.jpg",
+          description: "Terbuat dari bahan lateks yang sangat lembut",
+          price: 50000,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          student_id: "45615",
+          product_name: "T-shirt Polo",
+          product_image: "src/assets/img/1.jpg",
+          description: "Terbuat dari bahan katun yang sangat lembut",
+          price: 30000,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        }
+      ], {});
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
